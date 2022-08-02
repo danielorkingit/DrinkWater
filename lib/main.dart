c018afa7e0231acfdb089db1ec8270b4102bf272
danielorkingit <danielorkin@icloud.com>
2022-08-02 12:52:15 +0200

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:geocoder/geocoder.dart';
-import 'package:latlong/latlong.dart';
+import 'package:acquaintances/Pages/homescreen.dart';
+import 'package:acquaintances/Pages/settings.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,141 +16,6 @@ class MyApp extends StatelessWidget {
         '/': (context) => HomeScreen(),
         '/settings': (context) => Settings(),
       },
-    );
-  }
-}
-
-class Settings extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: () => Navigator.pop(context),
-          icon: Icon(
-            Icons.arrow_back_ios,
-            color: Colors.black,
-          ),
-        ),
-      ),
-      body: Center(
-        child: FlatButton(
-          child: Text('Pop!'),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  double long = 49.5;
-  double lat = -0.09;
-  LatLng point = LatLng(49.5, -0.09);
-  var location = [];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading: Icon(
-          Icons.panorama_fisheye_outlined,
-          color: Colors.blue,
-        ),
-        title: Text("Acquaintances",
-            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black)),
-        actions: [
-          IconButton(
-            onPressed: () => Navigator.pushNamed(context, '/settings'),
-            icon: Icon(
-              Icons.settings,
-              color: Colors.black,
-            ),
-          )
-        ],
-      ),
-      body: Stack(
-        children: [
-          FlutterMap(
-            options: MapOptions(
-              onTap: (p) async {
-                location = await Geocoder.local.findAddressesFromCoordinates(
-                    new Coordinates(p.latitude, p.longitude));
-
-                setState(() {
-                  point = p;
-                  print(p);
-                });
-
-                print(
-                    "${location.first.countryName} - ${location.first.featureName}");
-              },
-              center: LatLng(49.5, -0.09),
-              zoom: 5.0,
-            ),
-            layers: [
-              TileLayerOptions(
-                  urlTemplate:
-                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                  subdomains: ['a', 'b', 'c']),
-              MarkerLayerOptions(
-                markers: [
-                  Marker(
-                    width: 80.0,
-                    height: 80.0,
-                    point: point,
-                    builder: (ctx) => Container(
-                      child: Icon(
-                        Icons.location_on,
-                        color: Colors.red,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ],
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 34.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Card(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.all(16.0),
-                      hintText: "Search for your localisation",
-                      prefixIcon: Icon(Icons.location_on_outlined),
-                    ),
-                  ),
-                ),
-                Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Text(
-                            "${location.first.countryName},${location.first.locality}, ${location.first.featureName}"),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:alcoholcontroller/main.dart';
+import 'package:acquaintances/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
